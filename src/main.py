ef4e68f6d13b51ea629afe8af2538954d02b7e9c
Matthew Li <ml927@cornell.edu>
2017-03-11 16:28:44 -0500

    pushed in the main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from network_analysis import NetworkAnalysis
 from os import listdir
 from os.path import isfile, join
 
+
 def get_data_files(dir_path="../data"):
     ret = {"current": set(), "full": set()}
     for f in listdir(dir_path):
@@ -17,7 +18,27 @@ def get_data_files(dir_path="../data"):
     return ret
 
 
-
 if __name__ == '__main__':
-    pass
+    # Flags for control
+    currentOnly = True
+    noGame = True  # Only use the no game no life wiki. Intended for testing
 
+    # Setting datafiles to the correct files
+    data_files = set()
+    for (k, v) in get_data_files().items():
+        if not currentOnly:
+            data_files.update(v)
+        elif k == 'current':
+            data_files.update(v)
+    if noGame:
+        data_files = {f for f in data_files if "nogamenolife" in f}
+
+    # Processing networks
+    networks = {}
+    for f in data_files:
+        d = XMLParser(f).parse_to_dict()
+        net = NetworkParser.createGraphFromDict(d)
+        networks[f] = net
+
+    for (k, v) in networks.items():
+        NetworkAnalysis(v, k)
