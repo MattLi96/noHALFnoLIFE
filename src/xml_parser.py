ccc5b8d4294472dc289854836abf0c36cb1c834e
Matthew Li <ml927@cornell.edu>
2017-03-11 15:17:18 -0500

    made class

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -1,10 +1,23 @@
 import untangle
 
 
-file_name = '../data/nogamenolife_pages_current.xml'
-test_file = '../data/test.xml'
+class XMLParser:
+    def __init__(self):
+        pass
 
-file = open(file_name, 'r', encoding="utf8")
-obj = untangle.parse(file.read())
-for p in obj.mediawiki.page:
-    print(p.title.cdata)
+    def parse_to_obj(self, file_name):
+        file = open(file_name, 'r', encoding="utf8")
+        return untangle.parse(file.read())
+
+    def filter(self):
+        pass
+
+
+if __name__ == '__main__':
+    file_name = '../data/nogamenolife_pages_current.xml'
+    test_file = '../data/test.xml'
+
+    obj = XMLParser().parse_to_obj(file_name)
+
+    for p in obj.mediawiki.page:
+        print(p.title.cdata)
