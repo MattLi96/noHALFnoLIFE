a2398d079ef73c45e251d52b9e18bce4204a68d7
Matthew Li <ml927@cornell.edu>
2017-03-11 16:01:08 -0500

    moved get data files to main

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -2,20 +2,6 @@
 import sys
 import xmltodict
 import json
-from os import listdir
-from os.path import isfile, join
-
-
-def get_data_files(dir_path="../data"):
-    ret = {"current": set(), "full": set()}
-    for f in listdir(dir_path):
-        rel_path = join(dir_path, f)
-        if isfile(rel_path):
-            if f.endswith("_current.xml"):
-                ret['current'].add(rel_path)
-            elif f.endswith("_full.xml"):
-                ret['full'].add(rel_path)
-    return ret
 
 
 class XMLParser:
