a69879c91d77a85ecb2a1f88e4fc636a95244ee7
Matthew Li <ml927@cornell.edu>
2017-03-11 15:27:15 -0500

    added xmltodict

---
+++ src/network_parser.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+
+def convert(xml_file, xml_attribs=True):
+    with open(xml_file, "rb") as f:  # notice the "rb" mode
+        d = xmltodict.parse(f, xml_attribs=xml_attribs)
+        return d
+
+d = convert('../data/nogamenolife_pages_current.xml')
+print(d)
--- src/xml_parser.py
+++ src/xml_parser.py
@@ -29,3 +29,16 @@ if __name__ == '__main__':
 
         for p in obj.mediawiki.page:
             print(p.title.cdata)
+
+
+
+import xmltodict
+
+
+def convert(xml_file, xml_attribs=True):
+    with open(xml_file, "rb") as f:  # notice the "rb" mode
+        d = xmltodict.parse(f, xml_attribs=xml_attribs)
+        return d
+
+d = convert('../data/nogamenolife_pages_current.xml')
+print(d)
