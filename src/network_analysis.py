45aedb822bc5ca624bd7d6783930145734442e23
Austin Liu <atrix315@gmail.com>
2017-03-11 16:02:19 -0500

    starting up file for network analysis

---
+++ src/network_analysis.py
@@ -0,0 +1,10 @@
+import networkx as nx
+
+class NetworkAnalysis:
+    def __init__(self, G): #TODO any settings for the network analysis
+        self.G = G
+
+if __name__ == '__main__':
+    pass
+
+
