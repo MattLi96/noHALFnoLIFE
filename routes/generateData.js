e121347e341ddaad60b28141221983b02f881b0f
Austin Liu <atrix315@gmail.com>
2017-03-11 19:25:47 -0500

    adding rest endpoint

--- routes/generateData.js
+++ routes/generateData.js
@@ -1,10 +1,16 @@
 /*eslint-env node */
 var express = require('express');
 var router = express.Router();
+var PythonShell = require('python-shell');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send("Hello World")
+    PythonShell.run('src/main.py', function (err) {
+        if (err) throw err;
+        console.log('finished');
+    });
+    
+    res.send("Hello World");
 });
 
 module.exports = router;
