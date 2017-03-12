e5fb93b89a267ee44a92683aa4ae42c9ad0e27af
Austin Liu <atrix315@gmail.com>
2017-03-11 19:51:06 -0500

    terrified

---
+++ public/getOptions.js
@@ -0,0 +1,23 @@
+function reloadOptions(){
+    $.ajax({url: "./data/filelist", success: function(result){
+        result.forEach(function(el){
+            $("#options").append("<li><a href="+"#"+" class="+"dropDownListItem"+">" + result + "</a></li>");
+        });
+    }});
+}
+
+$(document).on('click', '.dropDownListItem', function(e) {
+    console.log("derp");
+    var name = e.currentTarget;
+    console.log(name.innerHTML);
+
+    var loadName = name.innerHTML.split("/")
+    loadName.shift()
+    loadName = loadName.join("/")
+
+    console.log(loadName)
+    generate(loadName)
+});
+
+reloadOptions()
+
