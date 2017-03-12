af018b40116b97f084eda0c2f47261cf6a4b8cdb
Austin Liu <atrix315@gmail.com>
2017-03-12 05:22:38 -0400

    hacking together a force solution

--- public/graphGen.js
+++ public/graphGen.js
@@ -1,3 +1,8 @@
+var forceConfig = {
+        worker: true,
+        barnesHutOptimize: true
+    };
+
 function generate(path) {
     $("#graph-container").html("")
 
@@ -25,25 +30,43 @@ function generate(path) {
         }
         sigma.renderers.def = sigma.renderers.canvas
         // Instantiate sigma:
-        s = new sigma({
+        window.s = new sigma({
             graph: g,
             container: 'graph-container'
         });
+
         // Initialize the dragNodes plugin:
-        var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+        var dragListener = sigma.plugins.dragNodes(window.s, window.s.renderers[0]);
         dragListener.bind('startdrag', function (event) {
-            console.log(event);
+            // console.log(event);
+            window.s.stopForceAtlas2();
         });
         dragListener.bind('drag', function (event) {
-            console.log(event);
+            // console.log(event);
         });
         dragListener.bind('drop', function (event) {
-            console.log(event);
+            // console.log(event);
         });
         dragListener.bind('dragend', function (event) {
-            console.log(event);
+            // console.log(event);
         });
     });
 }
 
+window.forceOn = false;
+
+$("#dance").on("click", function(){
+    if(window.forceOn){
+        window.s.stopForceAtlas2();
+    }
+    else{
+        window.s.startForceAtlas2(forceConfig);
+    }
+    window.forceOn = !window.forceOn;
+});
+
 generate("data/nogamenolife_pages_current.json")
+
+setTimeout(function(){
+    $("#dance").trigger("click");
+}, 200)
