bde5ec8aea12f35888091480f8e9deaed8f13db7
Matthew Li <ml927@cornell.edu>
2017-03-21 16:16:59 -0400

    Made zip better

--- zip_data.sh
+++ zip_data.sh
@@ -20,6 +20,11 @@ while getopts ":zuf" opt; do
 done
 
 pushd dataRaw
+pushd zipped
+cat x* > zipped.7z
+mv -f zipped.7z ..
+popd
+
 if ${zip} ; then
     7z u -mx zipped.7z *.xml
     cp -f zipped.7z zipped
@@ -27,14 +32,11 @@ if ${zip} ; then
     pushd zipped
     rm -f x*
     split -a 3 -b 100m zipped.7z
+    rm -f zipped.7z
     git add x*
     popd
 fi
 
-pushd zipped
 if ${unzip} ; then
-    cat x* > zipped.7z
-    mv -f zipped.7z ..
-    popd
     7z e zipped.7z
 fi
