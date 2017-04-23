dbf4a601987e4c9de1ef54fecf19962bbd786dcf
Kevin Jiang <kzj1995@yahoo.com>
2017-04-23 15:19:49 -0400

    Some print statements for tracking decentranlized search

--- src/decentralized_search.py
+++ src/decentralized_search.py
@@ -21,21 +21,27 @@ class HierarchicalDecentralizedSearch:
         """
         Uses decentralized search to get the path between two given nodes
         """
+        print(node1.name + " to " + node2.name)
         current_node = node1
         decentralized_search_path = []
         decentralized_search_path.append(current_node)
+        visited_nodes = set()
         while current_node != node2:
             current_neighbors = self.G.neighbors(current_node)
             min_distance = float("inf")
             min_distance_node = None
             for neighbor in current_neighbors:
-                if len(neighbor.categories) > 0:
+                if len(neighbor.categories) > 0 and (neighbor not in visited_nodes):
                     current_distance = self.get_hierarchy_distance(current_node, neighbor)
                     if current_distance < min_distance:
                         min_distance = current_distance
                         min_distance_node = neighbor
+            if min_distance_node is None:
+                while (min_distance_node is None) or (len(min_distance_node.categories) == 0):
+                    min_distance_node = current_neighbors[random.randint(0, len(current_neighbors) - 1)]
             decentralized_search_path.append(min_distance_node)
             current_node = min_distance_node
+            visited_nodes.add(current_node)
         return decentralized_search_path
 
     def run_decentralized_search(self, num_times):
@@ -48,14 +54,17 @@ class HierarchicalDecentralizedSearch:
         for i in range(num_times):
             node1 = None
             node2 = None
-            while node1 is None:
-                rand1_index = random.randint(0, len(nodes) - 1)
-                if len(nodes[rand1_index].categories) > 0:
-                    node1 = nodes[rand1_index]
-            while node2 is None:
-                rand2_index = random.randint(0, len(nodes) - 1)
-                if rand2_index != rand1_index and len(nodes[rand2_index].categories) > 0:
-                    node2 = nodes[rand2_index]
+            while (node1 is None) or (node2 is None) or (nx.has_path(self.G, node1, node2) == False):
+                while True:
+                    rand1_index = random.randint(0, len(nodes) - 1)
+                    if len(nodes[rand1_index].categories) > 0:
+                        node1 = nodes[rand1_index]
+                        break
+                while True:
+                    rand2_index = random.randint(0, len(nodes) - 1)
+                    if rand2_index != rand1_index and len(nodes[rand2_index].categories) > 0:
+                        node2 = nodes[rand2_index]
+                        break
             search_path = self.get_decentralized_search_path(node1, node2)
             print("Decentralized Search " + str(i + 1) + ": Length " + str(len(search_path)))
             decentralized_search_paths.append(search_path)
