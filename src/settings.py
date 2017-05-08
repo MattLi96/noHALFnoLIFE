12d74ff0d3094574641117986b13ee884c85432a
Matthew Li <ml927@cornell.edu>
2017-05-08 01:42:24 -0400

    clean up settings file slightly

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ output_path = "../output/"
 current_only = True  # Only use current files. Has no effect in time series mode
 no_game = True  # Only use the no game no life wiki. Intended for testing
 no_game_name = "nogamenolife"
-time_series = False  # If true do time series. Otherwise process file\
+time_series = False  # If true do time series. Otherwise process file
 
 generate_data = False
 
