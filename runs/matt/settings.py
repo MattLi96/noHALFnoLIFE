b7dc60ba9ac1f3c66e792ab7687f51c703ee77fb
Matthew Li <ml927@cornell.edu>
2017-05-10 01:29:50 -0400

    added a new run

--- runs/matt/settings.py
+++ runs/matt/settings.py
@@ -9,17 +9,17 @@ decentralized_search_settings = {
     "run_decentralized_search": True,
     "detailed_print": False,
     "hierarchy_nodes_only": True,
-    "widen_search": '2look',  # possible values are 'none', '2look', 'hierarchy'
-    "apply_weighted_score": True,
+    "widen_search": 'hierarchy',  # possible values are 'none', '2look', 'hierarchy'
+    "apply_weighted_score": False,
     "plots": True,
 }
 
-output_path = "../output/"
-public_data = "../data/overview/"
+output_path = "../data/matt/hierarchy_unweighted/"
+public_data = "../data/matt/hierarchy_unweighted/overview/"
 public_out_path = "../public/data/"
 
 current_only = True  # Only use current files. Has no effect in time series mode
-no_game = True  # Only use the no game no life wiki. Intended for testing
+no_game = False  # Only use the no game no life wiki. Intended for testing
 no_game_name = "nogamenolife"
 time_series = False  # If true do time series. Otherwise process file
 
