218f69324def1ebaf921cc7098a5bc341b51e5c1
Austin Liu <atrix315@gmail.com>
2017-05-25 18:01:34 -0400

    adding docstrings

--- utilities/convert_json_to_csv.py
+++ utilities/convert_json_to_csv.py
@@ -3,31 +3,42 @@ import json
 import os
 
 def get_json(path):
+    """
+    get_json finds the .nodeRemove object at the given path and returns the corresponding json object
+
+    Args:
+    path: the path location as a string
+
+    Returns:
+    The JSON object associated with the path
+    """
     pathAttempt = os.path.join(path, 'nodeRemove.json')
-    print("read from", pathAttempt)
     with open(pathAttempt, 'r') as json_data:
-        print("got file")
-        d = json.load(json_data)
-        return d
+        return json.load(json_data)
 
 def writeout(path):
+    """
+    writeout converts the nodeRemove JSON file into a CSV file within the same folder
+
+    Args:
+    path: the path location as a string
+    """
     pathName = os.path.join(path, 'node_removal.csv')
-    print(pathName)
     with open(pathName, 'w') as csvfile:
-        print("writing")
         fieldnames = ['removed', 'numNodes', 'numEdges', 'averageInDegree', 'averageOutDegree', 'selfLinks', 'averagePathLength', 'sizeLargest']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        print("about to write header")
         writer.writeheader()
-        
-        print("getting data")
         dataToWrite = get_json(path)
-        print("writing data")
         for i in range(len(dataToWrite.keys())):
             writer.writerow(dataToWrite[str(i)])
 
 def convert_dataset(path):
+    """
+    convert_dataset converts all nodeRemove JSON files in the given folder into CSV files
+
+    Args:
+    path: the path location of the folder as a string
+    """
     dirs = os.listdir( path )
     for dir in dirs:
         try:
@@ -35,6 +46,6 @@ def convert_dataset(path):
             writeout(pathAttempt)
             print("Finished " + dir)
         except:
-            print("Nah " + dir)
+            print("Could not write " + dir)
     
 convert_dataset("../data/full/none_unweighted")
