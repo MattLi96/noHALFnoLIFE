2b18bfe192d77a9f608aadaebea160436b361e16
Austin Liu <atrix315@gmail.com>
2017-05-25 17:17:51 -0400

    cleaning up the index.js file

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const express = require('express')
-const app = express()
+// Default requires for Express
+const express = require('express');
+const app = express();
 const router = express.Router();
 const path = require('path');
+
+// File for REST API endpoint /data
 const generateData = require('./routes/generateData');
 
+// Base Directory
 let basePath = __dirname;
-console.log(basePath)
 
+// Default Page
 router.get("/",function(req,res){
   res.sendFile(basePath + "/index.html");
 });
 
+// Make sure to include all files in /public
 app.use(express.static(__dirname + '/public'));
 
-app.use('/data', generateData)
+// Map /data to the REST API endpoint
+app.use('/data', generateData);
 app.use("/",router);
 
+// Make sure that the app can use other folders
 app.use('/src', express.static(__dirname + '/src'));
 app.use('/dataRaw', express.static(__dirname + '/dataRaw'));
 app.use('/output', express.static(__dirname + '/output'));
 
-let portnum = 8007
+// Port Number to start on
+let portnum = 8000;
 
+// Listen
 app.listen(portnum, function () {
-  console.log('Example app listening on port' + portnum + ' !')
+  console.log('Example app listening on PORT: ' + portnum + ' !');
 })
